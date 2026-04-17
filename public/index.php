4a02f2045015d6866014e1c93d6fbaaffff37801
BCpollito <victo.elcatire75@gmail.com>
2026-04-17 16:29:41 -0500

    fix: actualizando acceso a variable de entorno en caso de que el archivo no exista

--- public/index.php
+++ public/index.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__ . '/../src/Infrastructure/Config/EnvLoader.php';
 // Se Cargan las variables de entorno desde la raíz
-EnvLoader::load(__DIR__ . '/../src/.env');
+$envPath = __DIR__ . '/../.env';
+
+if (file_exists($envPath)) {
+    EnvLoader::load($envPath);
+}
 
 // 1. Requerimos los archivos base de la carpeta Common
 require_once __DIR__ . '/../src/Common/Classloader.php';
